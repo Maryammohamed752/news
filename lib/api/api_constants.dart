13bd7e9e1ddd3759ad9014b12e0562f25049d61a
Dell <mariammohamedahmedahmed80@gmail.com>
2025-02-22 10:35:05 -0800

    * finish search view

--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -1,4 +1,5 @@
 class ApiConstants {
   static const String baseUrl = 'newsapi.org';
   static const String apiKey = '978b3429adfe40489f4b109207b1aec6';
+  static const String everythingRoute = '/v2/everything';
 }
--- lib/moduels/search/search_view.dart
+++ lib/moduels/search/search_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lottie/lottie.dart';
+import 'package:news_app_two/api/api_manager.dart';
 import 'package:news_app_two/model/NewsResponse.dart';
+import 'package:news_app_two/moduels/home/news/news_item.dart';
+import 'package:news_app_two/theme/color_palette.dart';
 
 class SearchView extends StatefulWidget {
   static const String routeName = 'search_view';
@@ -14,21 +17,45 @@ class SearchView extends StatefulWidget {
 
 class _SearchViewState extends State<SearchView> {
   List<News> news = [];
-  int maxPage = 0;
+  int maxResult = 0;
   int currentPage = 1;
   String? errorMessage = "";
+  TextEditingController controller = TextEditingController();
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        if (controller.text.isNotEmpty && maxResult > news.length) {
+          _search();
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
+        controller: scrollController,
         slivers: [
           SliverAppBar(
             leadingWidth: 0,
             leading: SizedBox(),
             toolbarHeight: 95,
             title: TextFormField(
+              onFieldSubmitted: (value) {
+                currentPage = 1;
+                maxResult = 0;
+              },
+              controller: controller,
               decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Theme.of(context).scaffoldBackgroundColor,
                   hintText: AppLocalizations.of(context)!.search,
                   suffixStyle: Theme.of(context).textTheme.labelMedium,
                   prefixIcon: Icon(
@@ -57,12 +84,61 @@ class _SearchViewState extends State<SearchView> {
                       borderSide: BorderSide(color: Colors.black26))),
             ),
           ),
-          if (news.isEmpty)
+          if (errorMessage != null)
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  errorMessage!,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              ),
+            ),
+          if (news.isEmpty && errorMessage == null)
             SliverToBoxAdapter(
               child: Lottie.asset("assets/animation/empty.json"),
+            ),
+          if (news.isNotEmpty)
+            SliverList.separated(
+              itemBuilder: (context, index) {
+                if (maxResult == news.length) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: ColorPalette.primaryColor,
+                    ),
+                  );
+                } else {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: NewsItem(
+                      news: news[index],
+                    ),
+                  );
+                }
+              },
+              separatorBuilder: (context, index) => SizedBox(
+                height: 16,
+              ),
+              itemCount:
+                  news.length < maxResult ? news.length + 1 : news.length,
             ),
         ],
       ),
     );
+  }
+
+  _search() async {
+    errorMessage = "";
+    try {
+      var response = await ApiManager.searchNews(
+          searchQuery: controller.text, pageNumber: currentPage);
+      news.addAll(response.articles ?? []);
+
+      currentPage++;
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      setState(() {});
+    }
   }
 }
